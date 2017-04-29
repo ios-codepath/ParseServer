546fc1b37702c159f4d3e8f49a8324b91c32281d
rcholic <ivytony@gmail.com>
2017-04-29 12:06:30 -0400

    fix for unauthorized user error

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'waitlessApp',
-  restKey: 'restKey',
-  javascriptKey: 'javascriptKey',
+  // restKey: 'restKey',
+  // javascriptKey: 'javascriptKey',
   masterKey: process.env.MASTER_KEY || 'waitlessMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
